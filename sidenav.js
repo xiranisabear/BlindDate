3e88a00f1c998e7a16ecf8f4483e984fd5ca3520
Xiran Yang <xiranyang@10-17-131-152.dynapool.wireless.nyu.edu>
2019-05-02 21:46:38 -0400

    fix

--- sidenav.js
+++ sidenav.js
@@ -1,78 +1,79 @@
-$(document).ready(function() {
+
+$(document).ready(function(){
   console.log("ready")
-  var cards = document.getElementsByClassName("cardcontainer");
-  console.log(cards)
-  var i;
-  for (i = 0; i < cards.length; i++) {
-    cards[i].style.display = "none";
-  }
+var cards = document.getElementsByClassName("cardcontainer");
+console.log(cards)
+var i;
+for (i = 0; i < cards.length; i++) {
+  cards[i].style.display = "none";
+}
 
-  // var lornacard = document.getElementById('lornacard');
-  $("#character1").click(function() {
-    console.log("hi");
-    cards[0].style.display = "block";
+// var lornacard = document.getElementById('lornacard');
+$("#character1").click(function(){
+console.log("hi");
+cards[0].style.display = "block";
+});
+
+$("#character2").click(function(){
+console.log("hi");
+cards[1].style.display = "block";
+});
+
+$("#character3").click(function(){
+console.log("hi");
+cards[2].style.display = "block";
+});
+
+$("#character4").click(function(){
+console.log("hi");
+cards[3].style.display = "block";
+});
+
+$("#character5").click(function(){
+console.log("hi");
+cards[4].style.display = "block";
+});
+
+$("#character6").click(function(){
+console.log("hi");
+cards[5].style.display = "block";
+});
+
+$("#character7").click(function(){
+console.log("hi");
+cards[6].style.display = "block";
+});
+
+$("#character8").click(function(){
+console.log("hi");
+cards[7].style.display = "block";
+});
+
+$(".closecard").click(function(){
+console.log(this);
+console.log("close"+cards);
+
+var i;
+for (i = 0; i < cards.length; i++) {
+  cards[i].style.display = "none";
+}
+
+});
+
+$(".flip").click(function(){
+  console.log(this);
+  $(this).parents(".card").toggleClass("flipped");
+});
+
+$("#chapterone").click(function(){
+window.location = 'chapter1.html';
   });
 
-  $("#character2").click(function() {
-    console.log("hi");
-    cards[1].style.display = "block";
-  });
+  $("#chaptertwo").click(function(){
+  window.location = 'chapter2.html';
+    });
 
-  $("#character3").click(function() {
-    console.log("hi");
-    cards[2].style.display = "block";
-  });
-
-  $("#character4").click(function() {
-    console.log("hi");
-    cards[3].style.display = "block";
-  });
-
-  $("#character5").click(function() {
-    console.log("hi");
-    cards[4].style.display = "block";
-  });
-
-  $("#character6").click(function() {
-    console.log("hi");
-    cards[5].style.display = "block";
-  });
-
-  $("#character7").click(function() {
-    console.log("hi");
-    cards[6].style.display = "block";
-  });
-
-  $("#character8").click(function() {
-    console.log("hi");
-    cards[7].style.display = "block";
-  });
-
-  $(".closecard").click(function() {
-    console.log(this);
-    console.log("close" + cards);
-
-    var i;
-    for (i = 0; i < cards.length; i++) {
-      cards[i].style.display = "none";
-    }
-
-  });
-
-  $(".flip").click(function() {
-    console.log(this);
-    $(this).parents(".card").toggleClass("flipped");
-  });
-
-  $("#chapterone").click(function() {
-    window.location = 'chapter1.html';
-  });
-
-  $("#chaptertwo").click(function() {
-    window.location = 'chapter2.html';
-  });
-
-  $("#chapterthree").click(function() {
+    $("#chapterthree").click(function(){
     window.location = 'chapter3.html';
-  });
+      });
 });
